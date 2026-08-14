173da6734dd2d93378283685c2992a86574c373a
BushLives <Kevinl2406@outlook.com>
2026-08-14 11:37:40 -0600

    Navbar, Home screen, Movie List & Footer

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav className="w-full bg-black">
+            <div>
+                <h1 className="text-lg md:text-2xl text-mist-50 font-bold text-center mt-2 md:mt-4">MR. MOVIES</h1>
+            </div>
+            <div className="flex justify-center text-white">
+                
+
+                <div className="flex gap-2">
+                    <Link href="/Movie" className="transition-colors hover:text-gray-600">Movies</Link>
+                    <Link href="/" className="transition-colors hover:text-gray-600">Home</Link>
+                    <div className="flex-1"></div>
+                </div>
+            </div>
+        </nav>
+    )
+}
